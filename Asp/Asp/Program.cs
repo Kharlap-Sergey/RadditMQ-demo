a5ec06a764cbe0eee7b67dbd7f0f521137dcab2d
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-04-12 15:03:28 +0300

    Try to add page view

--- Asp/Asp/Program.cs
+++ Asp/Asp/Program.cs
@@ -13,14 +13,14 @@ namespace Asp
     {
         public static void Main(string[] args)
         {
-            var rpcClient = new RpcClient();
+            //var rpcClient = new RpcClient();
 
-            Console.WriteLine(" [x] Requesting fib(30)");
-            var response = rpcClient.Call("30");
-
-            Console.WriteLine(" [.] Got '{0}'", response);
-            rpcClient.Close();
-            //CreateHostBuilder(args).Build().Run();
+            //Console.WriteLine(" [x] Requesting fib(30)");
+            //var response = rpcClient.Call("30");
+             
+            //Console.WriteLine(" [.] Got '{0}'", response);
+            //rpcClient.Close();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
